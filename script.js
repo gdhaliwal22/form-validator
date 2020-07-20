d2068e65e27d2f87156f13beeec20aac8be6680b
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-07-20 13:28:32 -0700

    create event listeners and email validation function

--- script.js
+++ script.js
@@ -18,12 +18,38 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 
+//Check email is valid
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 //Event listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+
   if (username.value === "") {
     showError(username, "Username is required");
   } else {
     showSuccess(username);
+  }
+
+  if (email.value === "") {
+    showError(email, "Username is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === "") {
+    showError(password, "Username is required");
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value === "") {
+    showError(password2, "Username is required");
+  } else {
+    showSuccess(password2);
   }
 });
